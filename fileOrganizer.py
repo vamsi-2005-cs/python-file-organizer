f3bb00d4e0da717ef9814f79d31fe6361bff1ba2
vamsi-2005-cs <venkatavamsijavvadi@gmail.com>
2026-05-28 23:26:42 +0530

    Successful

---
+++ fileOrganizer.py
@@ -0,0 +1,57 @@
+import os
+import shutil
+
+folder = input("Enter folder path to organize: ")
+
+file_types = {
+
+    "Images": [
+        ".jpg", ".jpeg", ".png",
+        ".gif", ".bmp", ".webp"
+    ],
+
+    "Videos": [
+        ".mp4", ".mkv", ".avi",
+        ".mov", ".flv"
+    ],
+
+    "Documents": [
+        ".pdf", ".docx", ".doc",
+        ".txt", ".pptx", ".xlsx"
+    ],
+
+    "Music": [
+        ".mp3", ".wav", ".aac"
+    ],
+
+    "Archives": [
+        ".zip", ".rar", ".tar",
+        ".gz"
+    ]
+}
+
+for file in os.listdir(folder):
+    path = os.path.join(folder, file)
+
+    if(os.path.isdir):
+        continue
+
+    extention = os.path.splitext(file)[1].lower()
+    moved = False
+
+    for category, extentions in file_types.items():
+        if extention in extentions:
+            target = os.path.join(folder, category)
+            os.makedirs(target, exist_ok=True)
+            shutil.move(path, os.path.join(target, file))
+            print(f"{file} -> {category}")
+            moved = True
+            break
+
+    if not moved:
+        others = os.path.join(folder, "Others")
+        os.makedirs(others, exist_ok=True)
+        shutil.move(path, os.path.join(others, file))
+        print(f"{file} -> {category}")
+        
+print("Done!")
